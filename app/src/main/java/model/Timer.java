8861af1479a68dbbd53aa6d5dc5c336d5733f30c
Efraim <efra2977@student.su.se>
2019-08-14 11:42:08 +0200

    Initial commit

---
+++ app/src/main/java/model/Timer.java
@@ -0,0 +1,13 @@
+package model;
+
+public class Timer {
+
+	//time (millisekunder?)
+	//section int (första 25 min = section 1, paus 5 min section 2, nästa 25 min = 3, paus = 4 ... efter 4 jobb och 3 paus blir det lång paus
+	//status = work, break, longbreak
+
+
+	Timer(){
+
+	}
+}
