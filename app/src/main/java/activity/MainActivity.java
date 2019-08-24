8118727532d608a98eee692344d620d2821562ac
Efraim <efra2977@student.su.se>
2019-08-24 13:34:03 +0200

    Comment out all camera related code to start over >:(

--- app/src/main/java/activity/MainActivity.java
+++ app/src/main/java/activity/MainActivity.java
@@ -43,13 +43,13 @@ public class MainActivity extends AppCompatActivity {
 		Button resetButton = findViewById(R.id.button_reset);
 		Button skipButton = findViewById(R.id.button_skip);
 
-		mImageView = (ImageView) findViewById(R.id.photo_view);
+		mImageView = findViewById(R.id.photo_view);
 
 		mContext = this.getApplicationContext();
 
 		//TODO Glöm inte fixa onSaveInstancestate etc.
 		mTimer = new Timer(this);
-		mCamera = new Camera(this);
+//		mCamera = new Camera(this);
 
 
 
@@ -89,26 +89,26 @@ public class MainActivity extends AppCompatActivity {
 		switch (item.getItemId()){
 			case R.id.button_camera:
 				//start camera
-				new Camera(this).startCamera();
+//				new Camera(this).startCamera();
 				return true;
 			default:
 				return super.onOptionsItemSelected(item);
 		}
 	}
 
-	@Override //TODO Jobba med att skriva bilden som kommer till enhetens minne istället. Kapitel 16
-	protected void onActivityResult(int requestCode, int resultCode, Intent data){
-		super.onActivityResult(requestCode, resultCode,data);
-
-		if(resultCode == RESULT_OK && requestCode == CAMERA_REQUEST_CODE){
-
-			Uri uri = FileProvider.getUriForFile(this,
-					"com.example.studytimer.fileprovider",
-					mCamera.getMPhotoFile());
-
-			this.revokeUriPermission(uri,Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-
-			mCamera.updatePhotoView();
+//	@Override //TODO Jobba med att skriva bilden som kommer till enhetens minne istället. Kapitel 16
+//	protected void onActivityResult(int requestCode, int resultCode, Intent data){
+//		super.onActivityResult(requestCode, resultCode,data);
+//
+//		if(resultCode == RESULT_OK && requestCode == CAMERA_REQUEST_CODE){
+//
+//			Uri uri = FileProvider.getUriForFile(this,
+//					"com.example.studytimer.fileprovider",
+//					mCamera.getMPhotoFile());
+//
+//			this.revokeUriPermission(uri,Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+//
+//			mCamera.updatePhotoView();
 
 			//Result from camera
 //			Bitmap cameraImage = (Bitmap) data.getExtras().get(MediaStore.EXTRA_OUTPUT); // rad 101
@@ -116,8 +116,8 @@ public class MainActivity extends AppCompatActivity {
 //			mImageView.setVisibility(View.INVISIBLE);//TODO FUNKAR
 
 
-		}
-	}
+//		}
+//	}
 
 
 	public void setCountdownText(String countdownText) {
@@ -128,8 +128,8 @@ public class MainActivity extends AppCompatActivity {
 	public void setStartStopButtonText(int buttonTextId){
 		mStartStopButton.setText(buttonTextId);
 	}
-
-	public void setImageView(Bitmap imageView) {
-		mImageView.setImageBitmap(imageView);
-	}
+//
+//	public void setImageView(Bitmap imageView) {
+//		mImageView.setImageBitmap(imageView);
+//	}
 }
--- app/src/main/java/model/Camera.java
+++ app/src/main/java/model/Camera.java
@@ -20,61 +20,62 @@ import static activity.MainActivity.CAMERA_REQUEST_CODE;
 
 public class Camera {
 
-	private MainActivity mMainActivity;
-	private File mPhotoFile;
-
-	public Camera(MainActivity mainActivity){
-		mMainActivity = mainActivity;
-		mPhotoFile = this.getPhotoFile(mMainActivity);
-	}
-
-	public UUID getId(){
-		UUID iD = null;
-		return iD.randomUUID();
-	}
-
-	public String getPhotoFilename(){
-		return "IMG_" + getId().toString() + ".jpg";
-	}
-
-	public File getPhotoFile(MainActivity mainActivity){
-		File filesDir = mainActivity.getFilesDir();
-		return new File(filesDir,getPhotoFilename());
-	}
-
-	public void startCamera(){
-		final Intent captureImage = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-
-		Uri uri = FileProvider.getUriForFile(mMainActivity,
-				"com.example.studytimer.fileprovider",
-				mPhotoFile);
-		captureImage.putExtra(MediaStore.EXTRA_OUTPUT, uri);
-
-		List<ResolveInfo> cameraActivities = mMainActivity
-				.getPackageManager().queryIntentActivities(captureImage,
-						PackageManager.MATCH_DEFAULT_ONLY);
-
-		for (ResolveInfo activity : cameraActivities) {
-			mMainActivity.grantUriPermission(activity.activityInfo.packageName,
-					uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-		}
-
-		mMainActivity.startActivityForResult(captureImage, CAMERA_REQUEST_CODE);
-	}
-
-	public void updatePhotoView(){
-		if(mPhotoFile == null || !mPhotoFile.exists()){
-			mMainActivity.setImageView(null);
-		}
-		else{
-			Bitmap bitmap = PictureUtils.getScaledBitmap(
-					mPhotoFile.getPath(), mMainActivity);
-			mMainActivity.setImageView(bitmap);
-
-		}
-	}
-
-	public File getMPhotoFile() {
-		return mPhotoFile;
-	}
+//	private MainActivity mMainActivity;
+//	private File mPhotoFile;
+//
+//	public Camera(MainActivity mainActivity){
+//		mMainActivity = mainActivity;
+//		mPhotoFile = getPhotoFile(mMainActivity);
+//	}
+//
+//	public UUID getId(){
+//		UUID iD = null;
+//		return iD.randomUUID();
+//	}
+//
+//	public String getPhotoFilename(){
+//		return "IMG_" + getId().toString() + ".jpg";
+//	}
+//
+//	public File getPhotoFile(MainActivity mainActivity){
+//		File filesDir = mainActivity.getFilesDir();
+//		return new File(filesDir,getPhotoFilename());
+//	}
+//
+//	public void startCamera(){
+//		final Intent captureImage = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+//
+//		Uri uri = FileProvider.getUriForFile(mMainActivity,
+//				"com.example.studytimer.fileprovider",
+//				mPhotoFile);
+//		captureImage.putExtra(MediaStore.EXTRA_OUTPUT, uri);
+//
+//		List<ResolveInfo> cameraActivities = mMainActivity
+//				.getPackageManager().queryIntentActivities(captureImage,
+//						PackageManager.MATCH_DEFAULT_ONLY);
+//
+//		for (ResolveInfo activity : cameraActivities) {
+//			mMainActivity.grantUriPermission(activity.activityInfo.packageName,
+//					uri, Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
+//		}
+//
+//		mMainActivity.startActivityForResult(captureImage, CAMERA_REQUEST_CODE);
+//	}
+//
+//	public void updatePhotoView(){
+//		if(mPhotoFile == null || !mPhotoFile.exists()){
+//			mMainActivity.setImageView(null);
+//			System.out.println("mPhotoFile == null eller så finns den bara inte lolololololol");
+//		}
+//		else{
+//			Bitmap bitmap = PictureUtils.getScaledBitmap(
+//					mPhotoFile.getPath(), mMainActivity);
+//			mMainActivity.setImageView(bitmap);
+//
+//		}
+//	}
+//
+//	public File getMPhotoFile() {
+//		return mPhotoFile;
+//	}
 }
